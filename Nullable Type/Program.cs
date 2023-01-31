d5597eb33cbb6c714f243b6f8698bd7968408ed9
oluwaseunoa <oluwaseun.beicks@gmail.com>
2023-01-31 15:03:27 +0100

    Add project files.

---
+++ Nullable Type/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace TutorialsPoint_ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int? ticketsOnSale = 200;
+
+            int availableTickets = ticketsOnSale ?? 0;
+
+            Console.WriteLine("Available Ticket = {0}", availableTickets);
+
+            // int availableTickets;
+
+            //if(ticketOnSale == null)
+            //{
+            //    availableTickets = 0;
+            //}
+            //else
+            //{
+            //    availableTickets = (int)ticketOnSale;
+            //}
+
+            //Console.WriteLine("Available Ticket = {0}", availableTickets
+            //    );
+
+
+            //bool? areYouMajor = true;
+
+            //if (areYouMajor == true)
+            //{
+            //    Console.WriteLine("User is Major");
+            //}
+            //else if (areYouMajor == false) 
+            //{
+            //    Console.WriteLine("User not Major");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("User did not answer the question");
+            //}
+        }
+    }
+}
